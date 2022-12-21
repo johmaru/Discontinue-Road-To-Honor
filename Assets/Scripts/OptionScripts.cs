f5eab3c73c3e6dc892e56aadbdf93b9a4f1fdd77
Jo Matsumaru <hatunehimeka@outlook.jp>
2022-12-21 23:08:59 +0900

    解像度変更機能追加

--- Assets/Scripts/OptionScripts.cs
+++ Assets/Scripts/OptionScripts.cs
@@ -1,38 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class OptionScripts : MonoBehaviour
 {
-   [SerializeField] private Dropdown ResDropDown;
+ 
    public void OptionBack()
    {
       SceneManager.LoadScene(0);
    }
 
-   public void ResChange()
+  /* public void ResChange()
    {
+      resolutions = Screen.resolutions;
 
+      ResDropDown = GetComponent<Dropdown>();
 
-     var RDD = ResDropDown;
+      UnityAction<int> action = OnValueChanged;
+      
+      ResDropDown.onValueChanged.AddListener(action);
+   }
+   */
 
-      string selectedvalue = RDD.options[RDD.value].text;
-
-      if (selectedvalue == "1920x1080")
+  public void OnValueChanged(int value)
+   {
+      Debug.Log(value.ToString());
+      
+      if (value.ToString() == "0")
       {
-         Screen.SetResolution(1920, 1080, true);
+       Screen.SetResolution(1920, 1080, (FullScreenMode)60);   
       }
       
-      else if (selectedvalue == "1280x768")
+      else if (value.ToString() == "1")
       {
-         Screen.SetResolution(1280, 768, true);
+         Screen.SetResolution(1280, 768, (FullScreenMode)60);  
       }
       
-      else if (selectedvalue == "1024x768")
+      else if (value.ToString() == "2")
       {
-         Screen.SetResolution(1024, 768, true);
+         Screen.SetResolution(1024, 768, (FullScreenMode)60);
       }
+      
+      
    }
 }
